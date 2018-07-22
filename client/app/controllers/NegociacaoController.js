54dce4b1450f72451e3c447212d9d0d531c838b7
alyoshas <evelynschmitz1@gmail.com>
2018-07-22 15:14:51 -0300

    3.3 Associando métodos do controller às ações do user

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,7 +1,19 @@
 class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
+    //busca elementos:
+    // let inputData = document.querySelector( '#data' );
+    // let inputQuantidade = document.querySelector( '#quantidade' );
+    // let inputValor = document.querySelector( '#valor' );
 
-    alert('Chamei a ação no controller');
+    // realizando o bind, $ mantém document como seu contexto this
+    let $ = document.querySelector.bind(document);
+    let inputData = $('#data');
+    let inputQuantidade = $('#quantidade');
+    let inputValor = $('#valor');
+
+    console.log( inputData.value );
+    console.log( parseInt(inputQuantidade.value) );
+    console.log( parseFloat(inputValor.value) );
   }
 }
