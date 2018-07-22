6688a7fa53188079a0f09c26410b3fee7a0f69f2
alyoshas <evelynschmitz1@gmail.com>
2018-07-22 15:29:55 -0300

    3.4 Evitando percorrer desnecessariamente o DOM

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,19 +1,15 @@
 class NegociacaoController {
+  constructor() {
+    let $ = document.querySelector.bind(document);
+    this._inputData = $('#data');
+    this._inputQuantidade = $('#quantidade');
+    this._inputValor = $('#valor');
+  }
   adiciona(event) {
     event.preventDefault();
-    //busca elementos:
-    // let inputData = document.querySelector( '#data' );
-    // let inputQuantidade = document.querySelector( '#quantidade' );
-    // let inputValor = document.querySelector( '#valor' );
 
-    // realizando o bind, $ mantém document como seu contexto this
-    let $ = document.querySelector.bind(document);
-    let inputData = $('#data');
-    let inputQuantidade = $('#quantidade');
-    let inputValor = $('#valor');
-
-    console.log( inputData.value );
-    console.log( parseInt(inputQuantidade.value) );
-    console.log( parseFloat(inputValor.value) );
+    console.log( this._inputData.value );
+    console.log( parseInt( this._inputQuantidade.value ) );
+    console.log( parseFloat( this._inputValor.value ) );
   }
 }
